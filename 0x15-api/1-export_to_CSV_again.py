398073febfdb86b71659301ce3b9be63f1329189
AlvinOt <thisisalvinhere@gmail.com>
2023-07-26 23:15:55 +0000

    api tasks

--- 0x15-api/1-export_to_CSV_again.py
+++ 0x15-api/1-export_to_CSV_again.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     response = requests.get(todoUrl)
     tasks = response.json()
 
-    with open('{}.csv'.format(employeeId), 'w') as file:
+    with open('newname.csv'.format(), 'w') as file:
         for task in tasks:
             file.write('"{}","{}","{}","{}"\n'
                        .format(employeeId, username, task.get('completed'),
